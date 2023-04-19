e09e5e7b44ecdaed231f1538427d5cb8d5d392b4
Yuki Watanabe <ukwhatn@gmail.com>
2023-04-19 12:55:04 +0900

    Implant routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # root
+  root 'home#index'
+
+  # oauth
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+
+  scope :register do
+    get "1" => "registerations#step1"
+  end
+
+  get 'log_out', to: 'sessions#destroy', as: 'log_out'
 end
